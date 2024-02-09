27648ee8817e14a295d28fe9e13fa8b005a54be3
foldedtoad <foldedtoad@gmail.com>
2024-02-08 22:16:19 -0800

    add backlight config support

--- src/main.c
+++ src/main.c
@@ -20,154 +20,159 @@ LOG_MODULE_REGISTER(app);
 
 static uint32_t count;
 
-static struct gpio_dt_spec backlight = 
-		GPIO_DT_SPEC_GET_OR(DT_ALIAS(backlight), gpios, {0});
-
-#define BACKLIGHT_GPIO_DEV  DT_NODELABEL(gpio1)
-#define BACKLIGHT_PIN       DT_GPIO_PIN(DT_ALIAS(backlight), gpios)
-#define BACKLIGHT_FLAGS     DT_GPIO_FLAGS(DT_ALIAS(backlight), gpios)	
-
-static const struct device * gpio1;
-
 #ifdef CONFIG_GPIO
 static struct gpio_dt_spec button_gpio = 
-		GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
+        GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
 static struct gpio_callback button_callback;
 
 static void button_isr_callback(const struct device *port,
-				struct gpio_callback *cb,
-				uint32_t pins)
+                struct gpio_callback *cb,
+                uint32_t pins)
 {
-	ARG_UNUSED(port);
-	ARG_UNUSED(cb);
-	ARG_UNUSED(pins);
+    ARG_UNUSED(port);
+    ARG_UNUSED(cb);
+    ARG_UNUSED(pins);
 
-	count = 0;
+    count = 0;
 }
 #endif /* CONFIG_GPIO */
 
 #ifdef CONFIG_LV_Z_ENCODER_INPUT
 static const struct device *lvgl_encoder =
-	DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_encoder_input));
+    DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_encoder_input));
 #endif /* CONFIG_LV_Z_ENCODER_INPUT */
 
 static void lv_btn_click_callback(lv_event_t *e)
 {
-	ARG_UNUSED(e);
+    ARG_UNUSED(e);
 
-	count = 0;
+    count = 0;
 }
 
 static int set_backlight_on(void)
 {
-	gpio1 = DEVICE_DT_GET(BACKLIGHT_GPIO_DEV);
-    if (!gpio1) {
-        LOG_ERR("\"%s\" not found.", DEVICE_DT_NAME(BACKLIGHT_GPIO_DEV));
+#if defined(CONFIG_BACKLIGHT_FIRMWARE)
+ 
+    #define BACKLIGHT_GPIO_DEV  DT_NODELABEL(gpio0)
+    #define BACKLIGHT_PIN       DT_GPIO_PIN(DT_ALIAS(backlight), gpios)
+    #define BACKLIGHT_FLAGS     DT_GPIO_FLAGS(DT_ALIAS(backlight), gpios)
+
+    static const struct device * gpio;
+
+    LOG_INF("Backlight firmware configured");
+
+    gpio = DEVICE_DT_GET(BACKLIGHT_GPIO_DEV);
+    if (!gpio) {
         return -1;
     }
 
-    LOG_INF("Backlight: pin %d, flag %d", BACKLIGHT_PIN, BACKLIGHT_FLAGS);
+    gpio_pin_configure(gpio, BACKLIGHT_PIN, GPIO_OUTPUT);
+    gpio_pin_set(gpio, BACKLIGHT_PIN, 1);
 
-    int flags = (GPIO_OUTPUT | BACKLIGHT_FLAGS);
+    return 0;
 
-    gpio_pin_configure(gpio1, BACKLIGHT_PIN, flags);
+#elif defined(CONFIG_BACKLIGHT_MANUAL)
 
-	gpio_pin_set(gpio1, BACKLIGHT_PIN, 1);
+    LOG_INF("Backlight manually configured");
+    return 0;
 
-	LOG_INF("Backlight is ready");
+#else
 
-	return 0;
+    LOG_ERR("Set Backlight configuration method");
+    return 0     
+
+#endif
 }
 
 int main(void)
 {
-	char count_str[11] = {0};
-	const struct device *display_dev;
-	lv_obj_t *hello_world_label;
-	lv_obj_t *count_label;
+    char count_str[11] = {0};
+    const struct device *display_dev;
+    lv_obj_t *hello_world_label;
+    lv_obj_t *count_label;
 
-	LOG_INF("main: start"); 
+    LOG_INF("main: start"); 
 
-	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
-	if (!device_is_ready(display_dev)) {
-		LOG_ERR("Device not ready, aborting");
-		return 0;
-	}
+    display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    if (!device_is_ready(display_dev)) {
+        LOG_ERR("Device not ready, aborting");
+        return 0;
+    }
 
-	if (set_backlight_on() < 0) {
-		LOG_ERR("Backlight failure: aborting");
-		return 0;
-	}
+    if (set_backlight_on() < 0) {
+        LOG_ERR("Backlight failure: aborting");
+        return 0;
+    }
 
 #ifdef CONFIG_GPIO
-	if (gpio_is_ready_dt(&button_gpio)) {
-		int err;
+    if (gpio_is_ready_dt(&button_gpio)) {
+        int err;
 
-		err = gpio_pin_configure_dt(&button_gpio, GPIO_INPUT);
-		if (err) {
-			LOG_ERR("failed to configure button gpio: %d", err);
-			return 0;
-		}
+        err = gpio_pin_configure_dt(&button_gpio, GPIO_INPUT);
+        if (err) {
+            LOG_ERR("failed to configure button gpio: %d", err);
+            return 0;
+        }
 
-		gpio_init_callback(&button_callback, button_isr_callback,
-				   BIT(button_gpio.pin));
+        gpio_init_callback(&button_callback, button_isr_callback,
+                   BIT(button_gpio.pin));
 
-		err = gpio_add_callback(button_gpio.port, &button_callback);
-		if (err) {
-			LOG_ERR("failed to add button callback: %d", err);
-			return 0;
-		}
+        err = gpio_add_callback(button_gpio.port, &button_callback);
+        if (err) {
+            LOG_ERR("failed to add button callback: %d", err);
+            return 0;
+        }
 
-		err = gpio_pin_interrupt_configure_dt(&button_gpio,
-						      GPIO_INT_EDGE_TO_ACTIVE);
-		if (err) {
-			LOG_ERR("failed to enable button callback: %d", err);
-			return 0;
-		}
-	}
+        err = gpio_pin_interrupt_configure_dt(&button_gpio,
+                              GPIO_INT_EDGE_TO_ACTIVE);
+        if (err) {
+            LOG_ERR("failed to enable button callback: %d", err);
+            return 0;
+        }
+    }
 #endif /* CONFIG_GPIO */
 
 #ifdef CONFIG_LV_Z_ENCODER_INPUT
-	lv_obj_t *arc;
-	lv_group_t *arc_group;
+    lv_obj_t *arc;
+    lv_group_t *arc_group;
 
-	arc = lv_arc_create(lv_scr_act());
-	lv_obj_align(arc, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_set_size(arc, 150, 150);
+    arc = lv_arc_create(lv_scr_act());
+    lv_obj_align(arc, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_size(arc, 150, 150);
 
-	arc_group = lv_group_create();
-	lv_group_add_obj(arc_group, arc);
-	lv_indev_set_group(lvgl_input_get_indev(lvgl_encoder), arc_group);
+    arc_group = lv_group_create();
+    lv_group_add_obj(arc_group, arc);
+    lv_indev_set_group(lvgl_input_get_indev(lvgl_encoder), arc_group);
 #endif /* CONFIG_LV_Z_ENCODER_INPUT */
 
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN) || IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
-		lv_obj_t *hello_world_button;
+    if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN) || IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
+        lv_obj_t *hello_world_button;
 
-		hello_world_button = lv_btn_create(lv_scr_act());
-		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
-		lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
-						NULL);
-		hello_world_label = lv_label_create(hello_world_button);
-	} else {
-		hello_world_label = lv_label_create(lv_scr_act());
-	}
+        hello_world_button = lv_btn_create(lv_scr_act());
+        lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
+        lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
+                        NULL);
+        hello_world_label = lv_label_create(hello_world_button);
+    } else {
+        hello_world_label = lv_label_create(lv_scr_act());
+    }
 
-	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+    lv_label_set_text(hello_world_label, "Hello world!");
+    lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
-	count_label = lv_label_create(lv_scr_act());
-	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+    count_label = lv_label_create(lv_scr_act());
+    lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
 
-	lv_task_handler();
-	display_blanking_off(display_dev);
+    lv_task_handler();
+    display_blanking_off(display_dev);
 
-	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count/100U);
-			lv_label_set_text(count_label, count_str);
-		}
-		lv_task_handler();
-		++count;
-		k_sleep(K_MSEC(10));
-	}
+    while (1) {
+        if ((count % 100) == 0U) {
+            sprintf(count_str, "%d", count/100U);
+            lv_label_set_text(count_label, count_str);
+        }
+        lv_task_handler();
+        ++count;
+        k_sleep(K_MSEC(10));
+    }
 }
